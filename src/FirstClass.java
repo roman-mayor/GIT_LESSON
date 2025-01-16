a5db2a6d68b1739a0e047566243e6ec2166ba32f
Roman Maiorov <maiorovroma98@gmail.com>
2025-01-16 15:46:55 +0300

    add changes to FirstClass

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -2,5 +2,11 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("this is first class");
         System.out.println("this is change"); //new changes
+
+        String s = "GIT";
+        String[]array = s.split("");
+        for (String string : array) {
+            System.out.print(string + " ");
+        }
     }
 }
