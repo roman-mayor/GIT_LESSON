6e2772c901633dabfde6981cc2f24a4e3c9eb46b
Roman Maiorov <maiorovroma98@gmail.com>
2025-01-16 16:15:33 +0300

    del NewClass

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -12,13 +12,7 @@ public class FirstClass {
         for (String string : array) {
             System.out.print(string + " ");
         }
-        NewClass nc = new NewClass();
-        System.out.println(nc.s); //new changes 2.0
     }
-}
-
-class NewClass{
-    final String s = "GIT";
 }
 
 class SecondClass{
