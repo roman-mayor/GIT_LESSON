2658b5160f4308659e7edb80e3615dfebd458410
Roman Maiorov <61579171+roman-mayor@users.noreply.github.com>
2025-01-16 15:36:36 +0300

    Update FirstClass.java
    add changes to FirstClass

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,5 +1,6 @@
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("this is first class");
+        System.out.println("this is change"); //new changes
     }
 }
