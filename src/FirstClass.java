9853dd50747b6f70bb9fa54f70a80719c1d41f05
Roman Maiorov <61579171+roman-mayor@users.noreply.github.com>
2025-01-16 15:48:05 +0300

    Update FirstClass.java
    add NewClass and sout in main method

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -2,5 +2,11 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("this is first class");
         System.out.println("this is change"); //new changes
+        NewClass nc = new NewClass();
+        System.out.println(nc.s); //new changes 2.0
     }
+}
+
+class NewClass{
+    final String s = "GIT";
 }
